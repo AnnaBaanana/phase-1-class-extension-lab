e677375fafa3e72213f14f4861c4d206ff5f7547
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-05-10 18:53:01 -0400

    adding working class extensions

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+
+class Polygon {
+    constructor(arr){ 
+        this.arr = arr 
+    }
+
+    get countSides() {
+        return this.arr.length
+    }
+
+    get perimeter() {
+        let p=0;
+        for (let i=0; i<this.arr.length; i++) {
+            p+=this.arr[i]
+        }
+        return p
+    }
+}
+
+const rumb = new Polygon([2,3,2,3])
+
+class Triangle extends Polygon {
+    get isValid() {
+        let isValid;
+        if (this.arr[0] + this.arr[1] > this.arr[2]){
+            if (this.arr[1] + this.arr[2] > this.arr[0]) {
+                if (this.arr[0] + this.arr[2] > this.arr[1]) {
+                    isValid = true
+                } else { isValid = false }
+            }
+        }
+        return isValid
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        let isValid;
+        if (this.arr[0]===this.arr[1]) {
+            if (this.arr[0]===this.arr[2]) {
+                if (this.arr[0]===this.arr[3]) {
+            isValid=true} else {isValid=false}}}
+            return isValid
+        }
+    get area() {
+        //console.log(this.isValid)
+        //if (this.isValid===true) {
+            return this.arr[0]*this.arr[1]
+        }
+    }
+
+
+
+const triangle = new Triangle([4,5,6])
+const triangleBad = new Triangle([4,5,1])
+
+const square = new Square([6,6,6,6])
